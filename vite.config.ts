8b42f72bb55db16ab4b84e61c764527bd60fc4ec
waiyantun25 <waiyantun.business@gmail.com>
2025-02-23 00:45:55 +0700

    feat(aliases): add path alias for services in TypeScript and Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
       "@components": "/src/components",
       "@assets": "/src/assets",
       "@schemas": "/src/schemas",
+      "@services": "/src/services",
     },
   },
   build: {
