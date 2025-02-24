ab906a0d6f566a0f8b3ce5bafe2103b6bbd84e2c
waiyantun25 <waiyantun.business@gmail.com>
2025-02-24 10:07:43 +0700

    feat(dashboard): improve RecentTable layout and enhance SalesSection icons

--- src/pages/logged-in/dashboard/components/RecentTable.tsx
+++ src/pages/logged-in/dashboard/components/RecentTable.tsx
@@ -16,20 +16,20 @@ export const RecentTable = ({
   const { theme } = useTheme();
   return (
     <div
-      className={`shadow-sm rounded-2xl p-5  lg:col-span-4 ${theme === "dark" ? "text-white bg-black" : "text-black bg-white"}`}
+      className={`shadow-sm rounded-2xl p-5 lg:col-span-4 ${theme === "dark" ? "text-white bg-black" : "text-black bg-white"}`}
     >
       <div className="flex justify-between items-center mb-3">
-        <h1 className="font-bold text-lg ">{title}</h1>
+        <h1 className="font-bold text-lg">{title}</h1>
         <a href="#" className="text-blue-500 text-sm">
           See all
         </a>
       </div>
       <div className="overflow-x-auto">
-        <table className="w-full border-collapse min-w-[320px] text-center">
+        <table className="w-full border-collapse min-w-[600px] text-left">
           <thead>
-            <tr className="text-left bg-gray-300 text-gray-600 text-sm">
-              <th className="py-2 px-3 w-[px]">Profile</th>
-              <th className="py-2 px-3 w-[20px]">City</th>
+            <tr className="bg-gray-300 text-gray-600 text-sm">
+              <th className="py-2 px-3">Profile</th>
+              <th className="py-2 px-3">City</th>
               <th className="py-2 px-3">Date</th>
               <th className="py-2 px-3">Status</th>
               <th className="py-2 px-3">Price</th>
@@ -38,28 +38,38 @@ export const RecentTable = ({
           <tbody>
             {data.map((order) => (
               <tr key={order.id} className="border-t border-gray-300">
-                <td className="py-3 px-3 flex items-center space-x-3">
+                <td className="py-3 px-3 flex items-center space-x-3 whitespace-nowrap">
                   <img
                     src={order.img}
                     alt={order.name}
                     className="w-8 h-8 rounded-full"
                   />
-                  <span className="">{order.name}</span>
+                  <span className="text-sm">{order.name}</span>
                 </td>
-                <td className="py-3 px-3">
-                  <MapPinIcon className="text-red-500 w-4 h-4 my-auto" />
-                  <span className="">{order.city}</span>
+
+                <td className="py-3 px-3 whitespace-nowrap">
+                  <div className="flex items-center space-x-1">
+                    <MapPinIcon className="text-red-500 w-4 h-4" />
+                    <span className="text-sm">{order.city}</span>
+                  </div>
                 </td>
-                <td className="py-3 px-3 ">{order.date}</td>
-                <td className="py-3 px-3 flex items-center space-x-1">
+
+                <td className="py-3 px-3 text-sm whitespace-nowrap">
+                  {order.date}
+                </td>
+
+                <td className="py-3 px-3 flex items-center space-x-1 whitespace-nowrap">
                   {order.status === "Delivered" ? (
                     <CheckCircleIcon className="text-green-500 w-4 h-4" />
                   ) : (
                     <XCircleIcon className="text-red-500 w-4 h-4" />
                   )}
-                  <span className="">{order.status}</span>
+                  <span className="text-sm">{order.status}</span>
                 </td>
-                <td className="py-3 px-3 font-semibold ">{order.price}</td>
+
+                <td className="py-3 px-3 font-semibold text-sm whitespace-nowrap">
+                  {order.price}
+                </td>
               </tr>
             ))}
           </tbody>
