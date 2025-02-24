814daf713c620ef3c90298880e4cdbaaba84acda
waiyantun25 <waiyantun.business@gmail.com>
2025-02-24 15:38:05 +0700

    feat(api): configure loginApi

--- src/services/api/store.ts
+++ src/services/api/store.ts
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { dashboardApi } from "@services/api/dashboardApi";
+import { loginApi } from "@services/api/loginApi";
 
 const store = configureStore({
   reducer: {
     [dashboardApi.reducerPath]: dashboardApi.reducer,
+    [loginApi.reducerPath]: loginApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(dashboardApi.middleware),
+    getDefaultMiddleware().concat(dashboardApi.middleware, loginApi.middleware),
 });
 
 export default store;
