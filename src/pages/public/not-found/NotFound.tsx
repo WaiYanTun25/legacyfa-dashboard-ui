ffecf567c39a23c4ae776570ec44edc4591d2bc0
waiyantun25 <waiyantun.business@gmail.com>
2025-02-24 21:21:19 +0700

    refactor: Ui of Not found screen

--- src/pages/public/not-found/NotFound.tsx
+++ src/pages/public/not-found/NotFound.tsx
@@ -1,30 +1,30 @@
+import { useTheme } from "@src/hooks";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
 export const NotFound = () => {
   const navigate = useNavigate();
+  const { theme } = useTheme();
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900 transition-colors duration-300">
-      {}
-      <div className="max-w-md w-full p-6 bg-white dark:bg-gray-800 rounded-lg shadow-xl transform hover:scale-105 transition-transform duration-300">
-        {}
+    <div
+      className={` shadow-sm p-5 lg:ml-5 rounded-2xl flex h-[88vh] items-center justify-center transition-colors duration-300 ${theme === "dark" ? "bg-black text-white" : "bg-white text-black"}`}
+    >
+      <div className="max-w-md w-full p-6 rounded-lg shadow-xl transform hover:scale-105 transition-transform duration-300">
         <div className="flex items-center justify-center mb-6">
-          <h1 className="text-9xl font-bold text-primary dark:text-blue-400 animate-bounce">
+          <h1 className="text-9xl font-bold text-primary animate-bounce">
             404
           </h1>
         </div>
 
-        {}
-        <h2 className="text-2xl font-semibold text-gray-800 dark:text-gray-200 mb-4 text-center">
+        <h2 className="text-2xl font-semibold mb-4 text-center">
           Oops! Page Not Found
         </h2>
-        <p className="text-gray-600 dark:text-gray-400 mb-6 text-center">
+        <p className=" mb-6 text-center">
           It seems the page you’re looking for doesn’t exist or has been moved.
           Let’s get you back on track!
         </p>
 
-        {}
         <div className="flex items-center justify-center">
           <button
             onClick={() => navigate("/")}
